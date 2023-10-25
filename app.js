8f9ec207f7d0591b0e2678b41453c110e071c8a4
julesgaborit <jules.gaborit@my-digital-school.org>
2023-10-25 11:49:55 +0200

    add connection mongo

--- app.js
+++ app.js
@@ -1,36 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const mongoose = require('mongoose');
+const express = require('express');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-app.use(logger('dev'));
+const app = express();
+// view engine setup
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser()); 
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const CONNECTION_URL = 'mongodb+srv://starwars:yY0t3cQ6odtY8Ncs@starstar.1uealmw.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 16743;
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
